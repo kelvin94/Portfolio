71bdc4ef62229f6747e6aa027c320a592f98363b
root <root@DESKTOP-AF0E8A4.localdomain>
2020-08-03 12:44:37 -0700

    Updates

---
+++ precache-manifest.b44ffcaca982094ea2cfc082c377fbcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2da3330c28b739309d63bf5696b5f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbeb50216e9e8e01e572",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fed3bd9432edebb5b4ef",
+    "url": "/static/css/main.3d95756b.chunk.css"
+  },
+  {
+    "revision": "cbeb50216e9e8e01e572",
+    "url": "/static/js/2.1541ced4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1541ced4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed3bd9432edebb5b4ef",
+    "url": "/static/js/main.b91fc83e.chunk.js"
+  },
+  {
+    "revision": "6142ad8cca682da46c1c",
+    "url": "/static/js/runtime-main.eed53b22.js"
+  }
+]);
